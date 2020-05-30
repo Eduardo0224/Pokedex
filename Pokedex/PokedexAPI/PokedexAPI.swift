c5a3b71948146ea60ccff2888a660ea06b9500e8
Eduardo Andrade <eduardrade0224@gmail.com>
2020-05-30 09:57:21 -0500

    feat(app): :wrench: :new: Adds a PokedexAPI general class to manage all http request, both the conformation of complete urls and the errors handling

---
+++ Pokedex/PokedexAPI/PokedexAPI.swift
@@ -0,0 +1,129 @@
+//
+//  PokedexAPI.swift
+//  Pokedex
+//
+//  Created by Eduardo Andrade on 29/05/20.
+//  Copyright © 2020 Eduardo Andrade. All rights reserved.
+//
+
+import Foundation
+
+class PokedexAPI {
+    
+    // MARK: - Enums
+
+    /**
+     ## Manages the path component to complete the URL.
+     Makes use of associated values for such purpose
+     */
+    enum Endpoints {
+        static let base = "https://pokeapi.co/api/v2"
+
+        case getPokemonList
+        case getPokemon(String)
+        case getPredominantColor(String)
+        case getStats(String)
+        case getEvolutions(String)
+        case getMoves(String)
+
+        var stringURL: String {
+            switch self {
+            case .getPokemonList:
+                return "\(Endpoints.base)/pokemon/"
+            case .getPokemon(let id):
+                return "\(Endpoints.base)/pokemon/\(id)/"
+            case .getPredominantColor(let id):
+                return "\(Endpoints.base)/pokemon-color/\(id)/"
+            case .getStats(let id):
+                return "\(Endpoints.base)/stat/\(id)/"
+            case .getEvolutions(let id):
+                return "\(Endpoints.base)/evolution-chain/\(id)/"
+            case .getMoves(let id):
+                return "\(Endpoints.base)/move/\(id)/"
+            }
+        }
+
+        var url: URL? { URL(string: stringURL) }
+    }
+
+    /// NetworkError
+    enum NetworkError: Error {
+        case requestError(String?)
+        case internalServerError
+        case decodeError(String)
+        case unknown
+    }
+
+    private enum HTTPStatusCode: Int, Error {
+        case ok = 200
+        case notFound = 404
+        case internalServerError = 500
+        case unknown
+
+        init(code: Int) {
+            switch code {
+            case 200...299: self = .ok
+            case 400...499: self = .notFound
+            case 500...599: self = .internalServerError
+            default: self = .unknown
+            }
+        }
+    }
+
+    // MARK: - Functions
+    @discardableResult class func taskForGETRequest<RequestType: Decodable>(in url: URL,
+                                                                            response: RequestType.Type,
+                                                                            completion: @escaping (Result<RequestType, NetworkError>) -> Void) -> URLSessionTask {
+        let dataTask = URLSession.shared.dataTask(with: url) { data, urlResponse, requestError in
+
+            if let error = requestError {
+                callInMainThread {
+                    completion(.failure(.requestError(error.localizedDescription)))
+                }
+                return
+            } else {
+                guard let data = data else {
+                    callInMainThread {
+                        completion(.failure(.requestError(nil)))
+                    }
+                    return
+                }
+                let statusCode = HTTPStatusCode(code: (urlResponse as? HTTPURLResponse)?.statusCode ?? 0)
+                switch statusCode {
+                case .ok:
+                    let decoder = JSONDecoder()
+                    do {
+                        let responseDecoded = try decoder.decode(RequestType.self, from: data)
+                        callInMainThread {
+                            completion(.success(responseDecoded))
+                        }
+                    } catch (let error) {
+                        callInMainThread {
+                            completion(.failure(.decodeError(error.localizedDescription)))
+                        }
+                    }
+                case .notFound:
+                    callInMainThread {
+                        completion(.failure(.requestError(nil)))
+                    }
+                case .internalServerError:
+                    callInMainThread {
+                        completion(.failure(.internalServerError))
+                    }
+                case .unknown:
+                    callInMainThread {
+                        completion(.failure(.unknown))
+                    }
+                }
+            }
+        }
+        dataTask.resume()
+        return dataTask
+    }
+
+    private class func callInMainThread(_ completion: @escaping () -> Void) {
+        DispatchQueue.main.async {
+            completion()
+        }
+    }
+}
